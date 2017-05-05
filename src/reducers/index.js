d8a5c869e9435b22bcdfaebb69102703d80f02d5
GuroKung <greenblood11@hotmail.com>
2017-05-06 00:58:59 +0700

    show details

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
+// any key adds up as a key in global state
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'Javascript: The Good Parts'},
-        { title: 'Harry Potter'},
-        { title: 'The Dark Tower'},
-        { title: 'Ruby is fun'}
+        { title: 'Javascript: The Good Parts', pages: 101},
+        { title: 'Harry Potter', pages: 3 },
+        { title: 'The Dark Tower', pages: 50 },
+        { title: 'Ruby is fun', pages: 1 }
     ]
 }
